0f88ecfdaa144c20cdc8d90ce1adf1e4c3718c38
Ivan Storck <ivanoats@gmail.com>
2011-06-18 15:50:08 -0700

    heredocs

--- refinerygems.rb
+++ refinerygems.rb
@@ -2,20 +2,31 @@ require 'rubygems'
 require 'sinatra'
 require 'httparty'
 
+# a gem description returned from the rubygems.org api
 class RefineryGem
   include HTTParty
 end
 
 get '/' do
-  response = "<!DOCTYPE html>"
-  response << "<head><title>Refinery Gems</title></head><body>"
-  response << "<h1>RefineryGems</h1>\n"
-  response << "<ul>"
+  response = <<-EOF
+  <!DOCTYPE html>
+    <head>
+      <title>Refinery Gems</title>
+    </head>
+    <body>
+      <h1>RefineryGems</h1>
+      <ul>
+  EOF
   # call rubygems.org api
-  # http://rubygems.org/api/v1/search.json?query=refinerycms
   # Loop through and print out names of gems and links to their homepage
   RefineryGem.get('http://rubygems.org/api/v1/search.json?query=refinerycms').each do |g|
     response << "<li><a href=\"#{g['homepage_uri']}\">" + "#{g['name']}" + "</a>"
   end
-  response << "</ul></body></html>"
+  response << <<-EOF
+      </ul>
+      <p>By <a href="http://ivanstorck.com">Ivan Storck</a>
+    </body>
+  </html>
+  EOF
+  response
 end
