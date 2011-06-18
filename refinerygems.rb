32b62a579678cd3c3739e1bfab7df54eeed5e57c
Ivan Storck <ivanoats@gmail.com>
2011-06-18 15:30:30 -0700

    lists gems with word refinerycms in it on an html5 page

--- refinerygems.rb
+++ refinerygems.rb
@@ -1,6 +1,21 @@
 require 'rubygems'
 require 'sinatra'
+require 'httparty'
+
+class RefineryGem
+  include HTTParty
+end
 
 get '/' do
-  "RefineryGems"
+  response = "<!DOCTYPE html>"
+  response << "<head><title>Refinery Gems</title></head><body>"
+  response << "<h1>RefineryGems</h1>\n"
+  response << "<ul>"
+  # call rubygems.org api
+  # http://rubygems.org/api/v1/search.json?query=refinerycms
+  # Loop through and print out names of gems and links to their homepage
+  RefineryGem.get('http://rubygems.org/api/v1/search.json?query=refinerycms').each do |g|
+    response << "<li><a href=\"#{g['homepage_uri']}\">" + "#{g['name']}" + "</a>"
+  end
+  response << "</ul></body></html>"
 end
